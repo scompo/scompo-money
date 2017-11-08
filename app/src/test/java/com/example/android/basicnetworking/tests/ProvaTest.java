e853d953242664bd3c1859c2bde463829e68b63b
Mauro Scomparin <scompo@gmail.com>
2017-11-08 23:53:45 +0100

    Meh

---
+++ app/src/test/java/com/example/android/basicnetworking/tests/ProvaTest.java
@@ -0,0 +1,12 @@
+package com.example.android.basicnetworking.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ProvaTest{
+
+    @Test
+    public void testName() throws Exception {
+      assertTrue(true);
+    }
+}
